44a6ed652dd3aa9e42b0f57c2beb569769126a3c
oussamasf <59108871+oussamasf@users.noreply.github.com>
2020-12-22 17:23:19 +0100

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from __future__ import print_function, division
 from keras.models import Model
 from keras.layers import Concatenate, Add, Average, Input, Dense, Flatten, BatchNormalization, Activation, LeakyReLU
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D, Convolution2DTranspose
-from keras import keras.backend.image_data_format()as K
+from keras import backend as K
 from keras.utils.np_utils import to_categorical
 import keras.callbacks as callbacks
 import keras.optimizers as optimizers
